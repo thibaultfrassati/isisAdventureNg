9b0c005a87cdc4c22065da6301e90ae6eb1fdc7b
thibault <thibault.frassati@gmail.com>
2018-09-20 11:35:31 +0200

    correctifs

--- src2/app/bigvalue.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BigvaluePipe } from './bigvalue.pipe';
-
-describe('BigvaluePipe', () => {
-  it('create an instance', () => {
-    const pipe = new BigvaluePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
